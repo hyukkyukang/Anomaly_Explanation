fix: scaling error refactoring

src/DBAnomTransformer/data_factory/dataset/base.py
@@ -111,7 +111,9 @@ class AnomalyTransformerDataset(torch.utils.data.Dataset):
             segments_for_fitting = list_utils.do_flatten_list(
                 [
                     self.create_time_segments(
-                        d, win_size=self.win_size, anomaly_causes=self.anomaly_causes
+                        d,
+                        win_size=self.win_size,
+                        possible_anomaly_causes=self.anomaly_causes,
                     )
                     for d in data_for_fitting
                 ]
@@ -122,7 +124,9 @@ class AnomalyTransformerDataset(torch.utils.data.Dataset):
             time_segments = list_utils.do_flatten_list(
                 [
                     self.create_time_segments(
-                        d, win_size=self.win_size, anomaly_causes=self.anomaly_causes
+                        d,
+                        win_size=self.win_size,
+                        possible_anomaly_causes=self.anomaly_causes,
                     )
                     for d in data_list
                 ]
@@ -159,7 +163,7 @@ class AnomalyTransformerDataset(torch.utils.data.Dataset):
         self,
         data: AnomalyData,
         win_size: int,
-        anomaly_causes: Optional[List[str]] = None,
+        possible_anomaly_causes: Optional[List[str]] = None,
     ) -> List[TimeSegment]:
         segments: List[TimeSegment] = []
         total_time = len(data.values)
@@ -188,8 +192,8 @@ class AnomalyTransformerDataset(torch.utils.data.Dataset):
                 idx in data.valid_abnormal_regions
                 for idx in range(start_time, end_time)
             ]
-            if anomaly_causes:
-                cause = anomaly_causes.index(data.cause)
+            if possible_anomaly_causes:
+                cause = possible_anomaly_causes.index(data.cause)
             else:
                 cause = 0
             anomaly_cause = [cause if is_anomaly[idx] else 0 for idx in range(win_size)]

src/DBAnomTransformer/solver.py
@@ -367,17 +367,6 @@ def calculate_classification_accuracy(
     )
     cls_total_num_cnt = len(predicted_for_anomaly_regions)
 
-    (
-        cls_precision,
-        cls_recall,
-        cls_f_score,
-        cls_support,
-    ) = precision_recall_fscore_support(
-        gold_for_anomaly_regions,
-        predicted_for_anomaly_regions,
-        average="micro",
-    )
-
     return cls_correct_cnt, cls_total_num_cnt
 
 
@@ -819,23 +808,25 @@ class Solver(object):
             avg_train_loss = np.average(loss1_list)
 
             # Compute validateion loss and accuracy
-            vali_loss1, vali_loss2, vali_loss3, accuracy = self.vali(self.test_loader)
+            vali_loss1, vali_loss2, vali_loss3, cls_accuracy = self.vali(
+                self.test_loader
+            )
             self.model.train()
 
             logger.info(
-                "Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Vali acc: {4:.7f} ".format(
+                "Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Vali cause acc: {4:.7f} ".format(
                     epoch + 1,
                     len(self.train_loader),
                     avg_train_loss,
                     vali_loss1,
-                    accuracy,
+                    cls_accuracy,
                 )
             )
             early_stopping(
                 vali_loss1,
                 vali_loss2,
                 vali_loss3,
-                accuracy,
+                cls_accuracy,
                 self.model,
                 self.train_loader.dataset.scaler,
                 self.model_save_path,
@@ -908,9 +899,9 @@ class Solver(object):
             cls_correct_cnt += correct_cnt
             cls_num_cnt += total_cnt
 
-        accuracy = cls_correct_cnt / cls_num_cnt if cls_num_cnt else 0
+        cls_accuracy = cls_correct_cnt / cls_num_cnt if cls_num_cnt else 0
 
-        return np.average(loss_1), np.average(loss_2), np.average(loss_3), accuracy
+        return np.average(loss_1), np.average(loss_2), np.average(loss_3), cls_accuracy
 
     @torch.no_grad()
     def test(self):

test_inference.py
@@ -10,6 +10,7 @@ from omegaconf import OmegaConf
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
 from DBAnomTransformer.config.utils import default_config
+from DBAnomTransformer.data_factory.dataset.dbsherlock import ANOMALY_CAUSES
 from DBAnomTransformer.detector import DBAnomDector
 from DBAnomTransformer.solver import detect_adjustment
 
@@ -79,7 +80,11 @@ def read_in_dbsherlock_data() -> np.ndarray:
         values_as_df = pd.DataFrame(values, columns=data["attributes"][2:])
 
         # Run inference (detect anomaly)
-        anomaly_score, is_anomaly, anomaly_cause = detector.infer(data=values_as_df)
+        anomaly_score, is_anomaly, anomaly_cause = detector.infer(
+            data=values_as_df,
+            possible_anomaly_causes=ANOMALY_CAUSES,
+            tmp_gold_cause=data["cause"],
+        )
 
         ab_regions = data["abnormal_regions"]
         assert (
@@ -105,6 +110,24 @@ def read_in_dbsherlock_data() -> np.ndarray:
         print(
             f"After acc: {acc} precision: {precision} recall: {recall} f_score: {f_score}"
         )
+        # Compute cause classification accuracy
+        # Extract causes from only the anomaly regions
+        extracted_cause_pred = []
+        print(anomaly_cause)
+        for i in range(len(ab_regions)):
+            if ab_regions[i]:
+                extracted_cause_pred.append(anomaly_cause[i])
+        cause_gold = [ANOMALY_CAUSES.index(data["cause"]) for i in extracted_cause_pred]
+        # Compute accuracy
+        correct_cnt = sum(
+            [
+                1
+                for c_gold, c_pred in zip(cause_gold, extracted_cause_pred)
+                if c_gold == c_pred
+            ]
+        )
+        cause_acc = correct_cnt / len(cause_gold)
+        print(f"Cause_acc: {cause_acc}")
 
 
 def main():